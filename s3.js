2a6f0991fdc03e2216492024b91cc6259df19fe0
Chalon Lubin <chalonlubin1@gmail.com>
2023-02-01 11:14:57 -0800

    s3.js

--- s3.js
+++ s3.js
@@ -1,4 +1,6 @@
-const s3 = require('aws-sdk/clients/s3');
+require('dotenv').config()
+const fs = require('fs')
+const S3 = require('aws-sdk/clients/s3')
 
 const bucketName = process.env.AWS_BUCKET_NAME;
 const region = process.env.AWS_BUCKET_REGION;
@@ -8,7 +10,29 @@ const secretAccessKey = process.env.AWS_SECRET_KEY;
 const s3 = new S3({
   region,
   accessKeyId,
-  secretAccessKey
+  secretAccessKey,
 });
 
+// uploads a file to s3
+export function uploadFile(file) {
+  const fileStream = fs.createReadStream(file.path);
 
+  const uploadParams = {
+    Bucket: bucketName,
+    Body: fileStream,
+    Key: file.filename,
+  };
+  return s3.upload(uploadParams).promise();
+}
+
+// downloads a file from s3
+function getFileStream(fileKey) {
+  const downloadParams = {
+    Key: fileKey,
+    Bucket: bucketName
+  }
+
+  return s3.getObject(downloadParams).createReadStream()
+}
+
+exports.uploadFIle = uploadFile;
